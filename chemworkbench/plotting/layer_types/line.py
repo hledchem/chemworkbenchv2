2e5608b130d8720701a026ed133ecbc123281eb7
hledchem <haydenledvina@gmail.com>
2026-01-22 01:10:37 -0800

    Update line.py

--- chemworkbench/plotting/layer_types/line.py
+++ chemworkbench/plotting/layer_types/line.py
@@ -1,10 +1,15 @@
-def render_line(ax, layer):
+from chemworkbench.core.models import PlotLayerConfig
+
+
+def render_line(ax, layer: PlotLayerConfig):
     ax.plot(
         layer.x,
         layer.y,
         color=layer.color,
         linewidth=layer.linewidth,
         linestyle=layer.linestyle,
+        marker=layer.marker,
+        markersize=layer.markersize,
         alpha=layer.alpha,
         label=layer.label,
     )
