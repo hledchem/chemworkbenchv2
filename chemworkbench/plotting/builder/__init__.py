a48027b98931d86014c670517a72828392377a2e
hledchem <haydenledvina@gmail.com>
2026-01-22 22:23:58 -0800

    Create __init__.py

---
+++ chemworkbench/plotting/builder/__init__.py
@@ -0,0 +1,8 @@
+"""
+Builder layer for converting declarative plotting schemas into PlotConfig
+objects that the plotting engine can render.
+"""
+
+from .config_builder import PlotConfigBuilder
+
+__all__ = ["PlotConfigBuilder"]
