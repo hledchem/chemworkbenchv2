6b0f679f98ef2cc1487d855f26cdb82cf77393d7
hledchem <haydenledvina@gmail.com>
2026-01-24 09:32:13 -0800

    Create __init__.py

---
+++ chemworkbench/utils/file_sniffer/vendor/__init__.py
@@ -0,0 +1,19 @@
+from .agilent_detectors import AGILENT_DETECTORS
+from .bruker_detectors import BRUKER_DETECTORS
+from .thermo_detectors import THERMO_DETECTORS
+from .shimadzu_detectors import SHIMADZU_DETECTORS
+from .perkinelmer_detectors import PERKINELMER_DETECTORS
+from .waters_detectors import WATERS_DETECTORS
+from .jeol_detectors import JEOL_DETECTORS
+from .varian_detectors import VARIAN_DETECTORS
+
+__all__ = [
+    "AGILENT_DETECTORS",
+    "BRUKER_DETECTORS",
+    "THERMO_DETECTORS",
+    "SHIMADZU_DETECTORS",
+    "PERKINELMER_DETECTORS",
+    "WATERS_DETECTORS",
+    "JEOL_DETECTORS",
+    "VARIAN_DETECTORS",
+]
