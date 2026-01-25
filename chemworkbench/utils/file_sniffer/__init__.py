75f02dec20874302ed8e058a387a29c818144fb7
hledchem <haydenledvina@gmail.com>
2026-01-25 09:16:32 -0800

    Update __init__.py

--- chemworkbench/utils/file_sniffer/__init__.py
+++ chemworkbench/utils/file_sniffer/__init__.py
@@ -1,3 +1,9 @@
-from .file_sniffer import detect_file_type
+"""
+file_sniffer package
 
-__all__ = ["detect_file_type"]
+Public API surface for ChemWorkBench v2 file sniffing.
+"""
+
+from .file_sniffer import sniff_file
+
+__all__ = ["sniff_file"]
